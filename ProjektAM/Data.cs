4e95dfde78a64744d7cdb100c3c63125b52994fe
EddieTheHead <hubert.bossy@wp.pl>
2017-01-11 23:00:41 +0100

    szablon po c#

---
+++ ProjektAM/Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjektAM
+{
+    class JsnData
+    {
+        public List<bool> states { get; set; }
+    }
+}

6a1484fc8c6fa241871416449aeb908a87b46aca
Patryk Kotlarz <kotlarz.pat@gmail.com>
2017-01-12 08:33:42 +0100

    Dodanie statycznych resources na zewnątrz .jara

---
+++ Raspberry/src/main/java/pl/poznan/put/beans/BeansConfig.java
@@ -0,0 +1,13 @@
+package pl.poznan.put.beans;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class BeansConfig extends WebMvcConfigurerAdapter {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations("/resources/", "file:Web/");
+	}
+}
